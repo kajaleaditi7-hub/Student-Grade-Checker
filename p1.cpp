b0d43a3ef20bc295950be9a330b7d6114af4663f
kajaleaditi-hub <kajaleaditi7@gmail.com>
2026-08-17 10:22:09 +0530

    Add files via upload
    A simple  C++ Grade Checker that uses if-else statements to determine a student's grade based on their marks.

---
+++ p1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+
+    cout << "Enter your marks: ";
+    cin >> marks;
+
+    if (marks >= 90) {
+        cout << "Grade: A";
+    }
+    else if (marks >= 80) {
+        cout << "Grade: B";
+    }
+    else if (marks >= 70) {
+        cout << "Grade: C";
+    }
+    else if (marks >= 60) {
+        cout << "Grade: D";
+    }
+    else if (marks >= 40) {
+        cout << "Grade: E";
+    }
+    else {
+        cout << "Grade: F - Failed";
+    }
+
+    return 0;
+}
